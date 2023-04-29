7095c8c7a71af73fc4b3dedda74b86c328d0bf44
nazekov <nazekov.ul@gmail.com>
2023-04-29 14:06:23 +0600

    Changed AdminController
    - added addPerson()

--- src/main/java/com/example/test_security/controler/AdminController.java
+++ src/main/java/com/example/test_security/controler/AdminController.java
@@ -1,15 +1,34 @@
 package com.example.test_security.controler;
 
+import com.example.test_security.enums.Role;
+import com.example.test_security.enums.Status;
+import com.example.test_security.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping()
+    @GetMapping
     public String getAdminPanel() {
+        System.out.println("public String getAdminPanel() {");
         return "admin-panel";
+    }
+
+    @PostMapping("/add")
+    public String addPerson(@RequestParam Map<String, String> data) {
+        Person person = Person.builder()
+                        .name(data.get("name"))
+                        .role(Role.valueOf(data.get("role")))
+                        .email(data.get("email"))
+                        .status(Status.ACTIVE)
+                        .build();
+        System.out.println("Map<String, String> " + data);
+        return "redirect:/admin";
     }
 }
