10d98dd788877a62fadcb006139151eef6d7646c
nazekov <nazekov.ul@gmail.com>
2023-05-11 10:07:53 +0600

    Changed implementations in PersonDetails and UserDetailsServiceImpl

--- src/main/java/com/example/test_security/security/PersonDetails.java
+++ src/main/java/com/example/test_security/security/PersonDetails.java
@@ -1,75 +1,54 @@
 package com.example.test_security.security;
 
 import com.example.test_security.model.Person;
-import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
-import java.util.List;
 
-@Data
+@Getter
+@ToString
 public class PersonDetails implements UserDetails {
 
-    private final String username;
-    private final String password;
-    private final List<SimpleGrantedAuthority> authorities;
-    private final boolean isActive;
+    private final Person person;
 
-    public PersonDetails(String username,
-                         String password,
-                         List<SimpleGrantedAuthority> authorities,
-                         boolean isActive) {
-        this.username = username;
-        this.password = password;
-        this.authorities = authorities;
-        this.isActive = isActive;
+    public PersonDetails(Person person) {
+        this.person = person;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
+        return this.person.getRole().getAuthorities();
     }
 
     @Override
     public String getPassword() {
-        return password;
+        return this.person.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return username;
+        return this.person.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return isActive;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return isActive;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return isActive;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return isActive;
-    }
-
-    public static UserDetails fromPerson(Person person) {
-        return new org.springframework.security.core.userdetails.User(
-                person.getUsername(),
-                person.getPassword(),
-                true,
-                true,
-                true,
-                true,
-                person.getRole().getAuthorities()
-        );
+        return true;
     }
 }
--- src/main/java/com/example/test_security/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/test_security/security/UserDetailsServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.test_security.security;
 
-import com.example.test_security.model.Person;
 import com.example.test_security.repository.PersonRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,14 +17,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        System.out.println("UserDetails loadUserByUsername(String username)");
-        Person person = personRepository.findByUsername(username)
+        return new PersonDetails(personRepository.findByUsername(username)
                 .orElseThrow(() ->
-                        new UsernameNotFoundException("User doesn't exists")
-                );
-        System.out.println("ApplicationPerson " + person);
-        UserDetails userDetails = PersonDetails.fromPerson(person);
-        System.out.println("UserDetails " + userDetails);
-        return userDetails;
+                    new UsernameNotFoundException("Такой пользователь не существует.")
+                )
+            );
     }
 }
